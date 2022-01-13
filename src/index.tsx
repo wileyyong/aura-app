701c8915b35ea931cb899fa61c64a37d4c3ce340
phijfry <phijfry@protonmail.com>
2022-01-13 11:41:31 +0000

    feat: added SWR config

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import { SWRConfig } from 'swr';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SWRConfig
+      value={{
+        revalidateIfStale: true,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+      }}
+    >
+      <App />
+    </SWRConfig>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
