9fbc658120e643d1edd08bdf6fef4242286896c8
phijfry <phijfry@protonmail.com>
2022-01-18 18:52:36 +0000

    feat: added Footer component

---
+++ src/components/shared/Footer/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Grid, Typography, Box, Container, styled } from '@mui/material';
+
+const Wrapper = styled('div')`
+  background: ${({ theme }) => theme.palette.grey[200]};
+  padding: 40px 10px;
+  box-sizing: border-box;
+`;
+
+export const Footer = () => {
+  return (
+    <Wrapper>
+      <Container maxWidth="md">
+        <footer>
+          <Grid container>
+            <Grid item xs={3}>
+              <Box mb={2}>
+                <Typography variant="h6">
+                  <b>
+                    <u>AURA</u>
+                  </b>
+                </Typography>
+              </Box>
+              <Typography variant="caption">Supported by</Typography>
+              <Typography variant="h6">Balancer</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Box mb={2}>
+                <Typography>
+                  <b>Community</b>
+                </Typography>
+              </Box>
+              <Typography>Twitter</Typography>
+              <Typography>Discord</Typography>
+              <Typography>Telegram Chat</Typography>
+              <Typography>Telegram Announcements</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Box mb={2}>
+                <Typography>
+                  <b>Resources</b>
+                </Typography>
+              </Box>
+              <Typography>Documentation</Typography>
+              <Typography>Blog</Typography>
+              <Typography>Email</Typography>
+            </Grid>
+          </Grid>
+        </footer>
+      </Container>
+    </Wrapper>
+  );
+};
