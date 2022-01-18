efd3d9154184aca33a952eda1ea44774f7c6f296
0xeye <97349378+0xeye@users.noreply.github.com>
2022-01-18 21:39:38 +0000

    feat: downsize fonts

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,51 @@
 import { createTheme } from '@mui/material';
-import { purple } from '@mui/material/colors';
+import { blue } from '@mui/material/colors';
 
 export const theme = createTheme({
   palette: {
     primary: {
-      main: purple[500],
+      main: blue[500],
+    },
+    secondary: {
+      main: blue[100],
     },
   },
   myField: {
     myNestedField: '',
+  },
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        h1: {
+          fontSize: '4rem',
+        },
+        h2: {
+          fontSize: '2.75rem',
+        },
+        h3: {
+          fontSize: '2.25rem',
+        },
+        h4: {
+          fontSize: '1.25rem',
+        },
+        h5: {
+          fontSize: '1rem',
+        },
+        h6: {
+          fontSize: '0.75rem',
+        },
+      },
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+        disableTouchRipple: true,
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        disableElevation: true,
+      },
+    },
   },
 });
