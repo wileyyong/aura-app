1bfaf6a4567b09494f9ef556039f34cf3178f46a
phijfry <phijfry@protonmail.com>
2022-01-18 15:33:51 +0000

    feat: move staked to lock page

--- src/components/pages/Lock/index.tsx
+++ src/components/pages/Lock/index.tsx
@@ -1,18 +1,35 @@
 import React, { FC } from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { AccordionItem } from '../../shared/AccordionItem';
 import useLockedCVXAPR from '../../../hooks/useLockedCVXAPR';
+import useStakedCVXAPR from '../../../hooks/useStakedCVXAPR';
 
 export const Lock: FC = () => {
+  const { data: stakedCVXAPR } = useStakedCVXAPR();
   const lockedCVXAPR = useLockedCVXAPR();
 
   return (
     <Box>
-      <Box>
+      <Box mb={4}>
+        <Box mb={2}>
+          <Typography variant="h6">Lock AURA</Typography>
+        </Box>
         <AccordionItem
           poolId={0}
           symbol={'Locked AURA'}
           apr={lockedCVXAPR}
+          share={0}
+          expanded={true}
+        />
+      </Box>
+      <Box mb={4}>
+        <Box mb={2}>
+          <Typography variant="h6">Stake your AURA to earn auraBAL</Typography>
+        </Box>
+        <AccordionItem
+          poolId={0}
+          symbol={'AURA'}
+          apr={stakedCVXAPR}
           share={0}
           expanded={true}
         />
--- src/components/pages/Stake/index.tsx
+++ src/components/pages/Stake/index.tsx
@@ -3,11 +3,9 @@ import { Box, Typography } from '@mui/material';
 import { PoolsTable } from './PoolsTable';
 import { AccordionItem } from '../../shared/AccordionItem';
 import useStakedCVXCRVAPR from '../../../hooks/useStakedCVXCRVAPR';
-import useStakedCVXAPR from '../../../hooks/useStakedCVXAPR';
 
 export const Stake: FC = () => {
   const { data: stakedCVXCRVAPR } = useStakedCVXCRVAPR();
-  const { data: stakedCVXAPR } = useStakedCVXAPR();
 
   return (
     <Box>
@@ -26,18 +24,6 @@ export const Stake: FC = () => {
       <Box mb={4}>
         <Typography variant="h6">Stake Balancer LP Tokens</Typography>
         <PoolsTable />
-      </Box>
-      <Box mb={4}>
-        <Box mb={2}>
-          <Typography variant="h6">Stake your AURA to earn auraBAL</Typography>
-        </Box>
-        <AccordionItem
-          poolId={0}
-          symbol={'AURA'}
-          apr={stakedCVXAPR}
-          share={0}
-          expanded={true}
-        />
       </Box>
     </Box>
   );
