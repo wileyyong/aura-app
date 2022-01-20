3750a847218b85d7ce6b1b26f299e1185e58bf7b
0xeye <97349378+0xeye@users.noreply.github.com>
2022-01-20 18:54:18 +0000

    chore: update parseBN scaling

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,14 @@
 import { BigNumber } from 'ethers';
 
 export const shortAddress = (address: string) =>
-  address.substring(0, 4) +
-  '...' +
-  address.substring(address.length - 2, address.length);
+  address.substring(0, 4) + '...' + address.substring(address.length - 2, address.length);
 
 export const mediumAddress = (address: string) =>
-  address.substring(0, 8) +
-  '...' +
-  address.substring(address.length - 8, address.length);
+  address.substring(0, 8) + '...' + address.substring(address.length - 8, address.length);
 
-export const parseBN = (bn: BigNumber, scale?: number): number => {
-  const parsed = parseInt(bn.toString());
-  if (scale) return parsed / scale;
+export const parseBN = (bn?: BigNumber, scale?: number): number => {
+  const parsed = parseInt(bn?.toString() ?? '0');
+  if (scale) return parsed / 10 ** scale;
   return parsed;
 };
 
