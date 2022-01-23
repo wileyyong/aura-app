da6acb11cc2bb27a480f0efebc84a86df95a63d8
0xeye <97349378+0xeye@users.noreply.github.com>
2022-01-23 21:14:03 +0000

    chore: add Network enum

--- src/constants.ts
+++ src/constants.ts
@@ -14,8 +14,12 @@ interface Contracts {
   registry: string;
 }
 
+export enum Network {
+  EthereumMainnet = 1,
+}
+
 export const ADDRESS: { [key: number]: Contracts } = {
-  1: {
+  [Network.EthereumMainnet]: {
     multicall: '0xeefba1e63905ef1d7acba5a8513c70307c1ce441',
     cvx: '0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B',
     crv: '0xD533a949740bb3306d119CC777fa900bA034cd52',
