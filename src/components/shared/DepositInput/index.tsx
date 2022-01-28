72d87435c97dbf480c9de369dec90d126ea835f1
truelifedev <truelife1228@outlook.com>
2022-01-28 06:14:59 -0700

    fixed warning on the DepositInput

--- src/components/shared/DepositInput/index.tsx
+++ src/components/shared/DepositInput/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { Stack, Button } from '@mui/material';
 import { BigNumberish, ContractTransaction, ethers } from 'ethers';
